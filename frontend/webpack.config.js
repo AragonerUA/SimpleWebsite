6c2229c7daadf68dc597fcfb43e7dd795d87ec29
Aleksandr Komolkin <san4es.kom@gmail.com>
2023-07-04 15:46:33 +0300

    build

---
+++ frontend/webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    devtool: 'inline-source-map',
+    output: {
+        filename: "[name].[content hash].js",
+        path: path.resolve(__dirname, 'build'),
+        publicPath: "",
+        // clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: '/node_modules/'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: { importLoaders: 1 }
+                    },
+                    'postcss-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name].[hash][ext]',
+                }
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name].[hash][ext]',
+                }
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html')
+        }),
+        new MiniCssExtractPlugin()
+    ],
+    devServer: {
+        port: 63342,
+        open: true,
+        hot: true,
+        watchFiles: ['src/**/*.html']
+    }
+}
