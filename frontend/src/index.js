f6ab6772f30e61a23b1185a2316280d8d5564ada
Aleksandr Komolkin <san4es.kom@gmail.com>
2023-07-05 00:11:26 +0300

    Add index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,14 @@
-import './css/style.css'
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+function login() {
+    window.location = "pages/login.html"
+}
+
+function register() {
+    window.location = "pages/register.html"
+}
